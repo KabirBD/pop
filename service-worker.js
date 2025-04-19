2d61d3c1f9bf6c335be6eee76745f6317e074b93
Md. Kabir Hosen <mdkabirhosen786@gmail.com>
2025-04-19 19:33:56 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "popup-text-cache-v1";
+const FILES_TO_CACHE = [
+  "/pop",
+  "/pop/",
+  "/pop/index.html",
+  "/pop/style.css",
+  "/pop/script.js",
+  "/pop/icon-192.png",
+  "/pop/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
