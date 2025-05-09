1c2db351b9a827bc8390f6a7d570fec8ef03f64f
Md. Kabir Hosen <mdkabirhosen786@gmail.com>
2025-05-09 23:21:11 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,32 @@
-const CACHE_NAME = "popup-text-cache-v1";
+const CACHE_NAME = 'pop-up-text-v1';
 const FILES_TO_CACHE = [
-  "/pop",
-  "/pop/",
-  "/pop/index.html",
-  "/pop/style.css",
-  "/pop/script.js",
-  "/pop/icon-192.png",
-  "/pop/icon-512.png",
+  '/pop/',
+  '/pop/index.html',
+  '/pop/style.css',
+  '/pop/script.js',
+  '/pop/icon-192.png',
+  '/pop/icon-512.png',
+  '/pop/manifest.json'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
